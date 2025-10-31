32bc517af33e3e7194a891f030a9a9f1c6c3d925
katharye <multiden007@gmail.com>
2025-11-01 01:19:12 +0300

    put databases init on the top

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,10 @@
+from users import init_users_db
+from database import init_db
+
+init_users_db()
+init_db()
+
+
 import os
 import logging 
 import asyncio
@@ -16,7 +23,7 @@ dp = Dispatcher()
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
-
+    
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
