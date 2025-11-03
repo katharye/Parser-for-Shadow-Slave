9c26884f985c0fcad8d7da1e361841b18c2e3ef2
katharye <multiden007@gmail.com>
2025-11-03 13:29:34 +0300

    The bot and dispatcher were moved to another file to avoid cyclic imports

--- src/app/keyboards.py
+++ src/app/keyboards.py
@@ -1,37 +1,53 @@
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
-                           InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 from database import find_max_value, find_min_value
+from users import get_user_notifications_subscription
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Download chapters?'), KeyboardButton(text='Read here')],
-    [KeyboardButton(text='Notifications?')]
-],
-    resize_keyboard=True,
-    input_field_placeholder='Добро пожаловать в Мимика! Выберите действие:'
-)
+min_chapter = max_chapter = 992
 
-min_chapter = find_min_value()
-max_chapter = find_max_value()
+async def find_extreme_chapters():
+    global min_chapter, max_chapter 
+    min_chapter = find_min_value()
+    max_chapter = find_max_value()
+
+async def main(user_id):
+    keyboard = ReplyKeyboardBuilder()
+
+    # keyboard.add(KeyboardButton(text='Download chapters?'))
+    keyboard.add(KeyboardButton(text='Read here'))
+
+    if get_user_notifications_subscription(user_id):
+        keyboard.add(KeyboardButton(text='Notifications: ✅'))
+    else:
+        keyboard.add(KeyboardButton(text='Notifications: ❎'))
+
+
+    if user_id in [1498219587, 8264374103, 1180202795]:
+        keyboard.add(KeyboardButton(text='Rewrite ALL'))
+        keyboard.add(KeyboardButton(text='Update data'))
+    
+    return keyboard.adjust(2, 1, 2).as_markup(
+        resize_keyboard=True,
+        input_field_placeholder='Добро пожаловать в Мимика! Выберите действие:'
+    )
 
 async def navigation(current_chapter):
     keyboard = InlineKeyboardBuilder()
     
+    keyboard.add(InlineKeyboardButton(text='Choose a chapter', callback_data='choose'))
+
     if current_chapter > min_chapter:
         keyboard.add(InlineKeyboardButton(text='Previous', callback_data='prev'))
 
     if current_chapter < max_chapter:
         keyboard.add(InlineKeyboardButton(text='Next', callback_data='next'))
 
-    return keyboard.as_markup()
+    return keyboard.adjust(1, 2).as_markup()
 
-
-chapters = [str(i) for i in range(max_chapter, min_chapter - 1, -1)]
-async def reply_chapters():
-    global chapters
-    keyboard = ReplyKeyboardBuilder()
-    keyboard.add(KeyboardButton(text='Go back'))
-    for chapter in chapters:
-        keyboard.add(KeyboardButton(text=chapter))
-    return keyboard.adjust(1, 5).as_markup()
+go_back = ReplyKeyboardMarkup(
+    keyboard=[
+    [KeyboardButton(text='Go back')]
+    ],
+    resize_keyboard=True
+)
